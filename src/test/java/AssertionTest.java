ecc4ca76429761e1b5f5f5365f95ef912154c0e4
Kate <39049841+ryzhakova@users.noreply.github.com>
2022-12-21 17:58:20 +0300

    Add files via upload

---
+++ src/test/java/AssertionTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AssertionTest {
+    @Test
+    public void test(){
+        Assertions.assertTrue(false);
+    }
+}
